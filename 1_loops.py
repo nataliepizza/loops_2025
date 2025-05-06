2a19b798ed8a4e89bc07071b4313d78e4eb9382e
nataliepizza <ngutierrez33@cps.edu>
2025-05-06 12:00:25 -0500

    worked on loops

--- 1_loops.py
+++ 1_loops.py
@@ -4,8 +4,9 @@ fruits = ["apple", "banana", "cherry", "date"]
 
 # Challenge:
 # Use a for loop to print each fruit on a new line.
-
-
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
 # Given a list of school subjects:
 subjects = ["Math", "Science", "History", "Art"]
 
@@ -19,3 +20,30 @@ numbers = [5, 10, 15, 20]
 
 # Challenge:
 # Use a for loop to add all the numbers and print the total.
+# Example Practice:
+# Given this list of fruits:
+fruits = ["apple", "banana", "cherry", "date"]
+
+# Challenge:
+# Use a for loop to print each fruit on a new line.
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+# Given a list of school subjects:
+subjects = ["Math", "Science", "History", "Art"]
+
+# Challenge:
+# Use a for loop and range to print each subject along with its index:
+# Example output: "Subject 0: Math"
+
+for fruit in fruits : 
+    print(fruit)
+
+# Given:
+numbers = [5, 10, 15, 20]
+
+# Challenge:
+# Use a for loop to add all the numbers and print the total.
+subjects = ["math", "science", "history", "art"]
+for subjects in subjects:
+    print(subject)
